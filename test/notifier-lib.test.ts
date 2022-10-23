ee588762bb0c0a56ac5253c727537d2653bcc65a
Rob Bulmer <Robert.bulmer@cef.co.uk>
2022-10-23 12:50:45 +0100

    Initial commit

---
+++ test/notifier-lib.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NotifierLib from '../lib/notifier-lib-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/notifier-lib-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NotifierLib.NotifierLibStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
